5ee4a3d6f0a8fb28ccb9f3f492d50ab3f8eefcf4
Nye-cozy <cozynye@gmail.com>
2020-07-31 19:22:56 +0900

    nomadcoing

---
+++ router-tutorial/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>홈</h1>
+            <p>홈, 그 페이지는 먼저 보여주지</p>
+        </div>
+    );
+};
+
+export default Home;
